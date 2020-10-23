f413dca825bd0e4f00d948c5e61cef272bd79545
Warrior Defend <warriordefenderz@gmail.com>
2020-10-23 08:54:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
 
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPT License",
+        "License :: OSI Approved :: GNU License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
