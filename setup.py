c26cce0cd2bac7ad6e76202b50b56851a1afacb5
Sunrit Jana <warriordefenderz@gmail.com>
2020-11-13 17:38:57 +0530

    Change some publish rules, and fix setup.py properly to ensure no issues from next time.

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from pathlib import Path
 
 from hypixelio.utils.constants import VERSION
 
+BASE_DIR = Path(__file__).resolve().parent
+
+README = Path(BASE_DIR / "README.md").read_text()
+REQUIREMENTS = Path(BASE_DIR / "requirements.txt").read_text().splitlines()
+
 setuptools.setup(
     name="HypixelIO",
     version=VERSION,
@@ -11,22 +16,30 @@ setuptools.setup(
     author_email="warriordefenderz@gmail.com",
 
     description="A modern efficient and faster way of interacting with the Hypixel API!",
-    long_description=Path("README.md").read_text(),
+    long_description=README,
     long_description_content_type="text/markdown",
-    license="MIT",
+    license="GPL v3",
 
     url="https://github.com/janaSunrise/HypixelIO",
+
     packages=setuptools.find_packages(
         exclude=["tests", "tests.*", "tools", "tools.*"]
     ),
 
-    install_requires=Path("requirements.txt").read_text().split("\n"),
+    install_requires=REQUIREMENTS,
 
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+
+        "Programming Language :: Python :: Implementation :: CPython",
+
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: Implementation :: CPython"
     ],
-    python_requires='>=3.7',
+
+    python_requires='>=3.6',
 )
