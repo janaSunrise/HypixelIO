1006a86137cc9c092dd2156cf0135033a1ce203f
janaSunrise <warriordefenderz@gmail.com>
2021-03-11 12:20:43 +0530

    Tweak the init

--- hypixelio/__init__.py
+++ hypixelio/__init__.py
@@ -11,6 +11,12 @@ __license__ = "GPL-3.0 License"
 __copyright__ = "Copyright 2021 Sunrit Jana"
 
 __all__: t.Tuple[str, ...] = (
+    "__author__",
+    "__email__",
+    "__version__",
+    "__license__",
+    "__copyright__",
+
     "Caching",
     "CacheBackend",
     "Client",
