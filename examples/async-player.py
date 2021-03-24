cf6112355ebbffb7d08685c3082911aa9a070587
janaSunrise <warriordefenderz@gmail.com>
2021-03-25 00:15:47 +0530

    Added async example.

---
+++ examples/async-player.py
@@ -0,0 +1,24 @@
+import os
+from textwrap import dedent
+
+from hypixelio.ext.asyncio import AsyncClient
+
+
+async def fetch() -> None:
+    # Login to the API
+    client = AsyncClient(api_key=os.environ["HYPIXEL_KEY"])
+
+    # Get a player object
+    player = await client.get_player(name="janaSunrise")
+
+    # Extract the data from the object
+    name, uuid, achievements = player.NAME, player.UUID, player.ACHIEVEMENTS
+
+    # Print the data
+    print(
+        dedent(f"""
+        Name: {name}
+        UUID: {uuid}
+        Achievements: {achievements}
+        """)
+    )
