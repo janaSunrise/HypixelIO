98e3f598c5830a706923c8bd24d581837515741b
janaSunrise <warriordefenderz@gmail.com>
2021-10-29 17:48:31 +0530

    Use typing aliased as `t` instead.

--- docs/extensions/resourcelinks.py
+++ docs/extensions/resourcelinks.py
@@ -2,7 +2,7 @@
 # Copyright 2007-2020 by the Sphinx team
 # Licensed under BSD.
 
-from typing import Any, Dict, List, Tuple
+import typing as t
 
 import sphinx
 from docutils import nodes, utils
@@ -13,24 +13,28 @@ from sphinx.util.nodes import split_explicit_title
 from sphinx.util.typing import RoleFunction
 
 
-def make_link_role(resource_links: Dict[str, str]) -> RoleFunction:
+def make_link_role(resource_links: t.Dict[str, str]) -> RoleFunction:
     def role(
         typ: str,
         rawtext: str,
         text: str,
         lineno: int,
         inliner: Inliner,
-        options: Dict = {},  # noqa: B006
-        content: List[str] = []  # noqa: B006
-    ) -> Tuple[List[Node], List[system_message]]:
-
+        options: t.Dict = {},  # noqa: B006
+        content: t.List[str] = []  # noqa: B006
+    ) -> t.Tuple[t.List[Node], t.List[system_message]]:
         text = utils.unescape(text)
+
         has_explicit_title, title, key = split_explicit_title(text)
+
         full_url = resource_links[key]
+
         if not has_explicit_title:
             title = full_url
+
         pnode = nodes.reference(title, title, internal=False, refuri=full_url)
         return [pnode], []
+
     return role
 
 
@@ -38,7 +42,7 @@ def add_link_role(app: Sphinx) -> None:
     app.add_role('resource', make_link_role(app.config.resource_links))
 
 
-def setup(app: Sphinx) -> Dict[str, Any]:
+def setup(app: Sphinx) -> t.Dict[str, t.Any]:
     app.add_config_value('resource_links', {}, 'env')
     app.connect('builder-inited', add_link_role)
     return {'version': sphinx.__display_version__, 'parallel_read_safe': True}
