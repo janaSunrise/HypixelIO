2105bcf382c2355e81a64b83d22d1b87c6a2617a
janaSunrise <warriordefenderz@gmail.com>
2021-12-07 19:45:08 +0530

    Remove bandit, and pytest benchmark.

--- setup.py
+++ setup.py
@@ -67,7 +67,6 @@ setuptools.setup(
     # Classifiers
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
